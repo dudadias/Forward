5f2282652608a67e3b81d0edc95fce3c72e547fc
dudadias <diasnights@gmail.com>
2024-01-26 20:21:35 -0300

    primitive data types

--- variables/Main.java
+++ variables/Main.java
@@ -3,5 +3,22 @@ package variables;
 public class Main {
     public static void main(String[] args) {
 
+        System.out.println("Volleyball team subscription");
+        System.out.println("obs: just younger!");
+
+        String name = new String();
+        name = "Maria Eduarda";
+        int age = 20;
+        double height = 1.57;
+        boolean married = false;
+        char gender = 'F';
+
+        System.out.println("Married? R:" +married+  '\n' + "So let's continue your subscription");
+        System.out.println("Hello " +name+ '\n' + "I'm glad to see you here");
+        System.out.println("You are " +age+ " years old and Would you like to subscribe in voleeyball team, right?");
+        System.out.println("We are organize a female team, You are F or M? R: " +gender+ " great!");
+        System.out.println("Sorry but your subscription isn't possible, because you heigth is " + height + " and it's not suitable! You are short...");
+        System.out.println("Thank you!");
+
     }
 }
